eb55d196856105c56e3399242161a787610a8320
kunal <kunalsarkar515@gmail.com>
2022-01-14 16:07:52 +0530

    fixed kunal

--- app1/static/js/uploader.js
+++ app1/static/js/uploader.js
@@ -1,68 +1,87 @@
 function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== "") {
-      const cookies = document.cookie.split(";");
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        // Does this cookie string begin with the name we want?
-        if (cookie.substring(0, name.length + 1) === name + "=") {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
       }
     }
-    return cookieValue;
   }
+  return cookieValue;
+}
 $(document).on('click', '#savebtn', function (event) {
 
-    event.preventDefault();
-    console.log('clicked now ')
-    // var output = '';
-    const csrftoken = getCookie("csrftoken");
-    
-    
-    
+  event.preventDefault();
+  console.log('clicked now ')
+  // var output = '';
+  const csrftoken = getCookie("csrftoken");
 
 
 
-    var nm = $('#username').val();
-    console.log($('#ajax_image').val())
-    var fl = new FormData();
-    // var fils = $('#ajax_image')[0].files[0];
-    var fd = ('file', $('#ajax_image')[0].files[0].name);
-    fl.append('file' , $('#ajax_image')[0].files[0] )
-    // fl.append( 'input' , $('#username').val())
-    console.log('file', $('#ajax_image')[0].files)
-    // console.log(nm)
-    // console.log(fl)
-    console.log(fl)
 
-    var my_data
-    // console.log(nm)
-    if (nm == '') {
-        console.log('this is empty plz field ')
-    }
-    if (fd == '') {
-        console.log('this file also not upload')
-    }
-    else{
-        // console.log(my_data);
-        var filed = { uname : nm ,  file: fl} 
 
-        $.ajax({
-            url: "save_data",
-            method: "POST",
-            headers: { "X-CSRFToken": csrftoken },
-            data: filed,
-            contentType : false,
-            processData: false,
-            success: function (data){
-                console.log(data)
-            }
-            
-        })
+
+  var nm = $('#username').val();
+  console.log($('#ajax_image').val())
+  var fl = new FormData();
+  // var fils = $('#ajax_image')[0].files[0];
+  var fd = ('file', $('#ajax_image')[0].files[0].name);
+  fl.append('file_name', $('#ajax_image')[0].files[0].name);
+  fl.append('file', $('#ajax_image')[0].files[0])
+  fl.append('name', nm)
+  // fl.append( 'input' , $('#username').val())
+  console.log('file', $('#ajax_image')[0].files)
+  // console.log(nm)
+  // console.log(fl)
+  console.log(fl)
+
+  // var my_data
+  // console.log(nm)
+  if (nm == '') {
+    console.log('this is empty plz field ')
+  }
+  if (fd == '') {
+    console.log('this file also not upload')
+  }
+  else {
+    // console.log(my_data);
+    var filed = { uname: nm, file: fl }
+
+    $.ajax({
+      url: "save_data",
+      method: "POST",
+      headers: { "X-CSRFToken": csrftoken },
+      data: fl,
+      contentType: false,
+      processData: false,
+      mimetype: 'multipart/form-data',
+      success: function (data) {
+        console.log(data)
+        console.log(data['client_data']['id'])
+        console.log(data.client_data)
+        for(i in data.client_data){
+
+          var add_img = `<tr>
+                <td>${data.client_data[i].id}</td>
+                <td>${data.client_data[i].user_name}</td>
+                <td class="img_style"> <img src="${data.client_data[i].image}" alt=""> </td>
+            </tr>`
         
-    }
+        }
+        
+        $('#tbody').append(add_img)
+        $('#ajax_image').val('')
+        $('#username').val('')
+
+      }
+
+    })
+
+  }
 })
 
 // $(function() {
--- app1/views.py
+++ app1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from . models import *
 from django.http import JsonResponse
 from django.core.files.storage import FileSystemStorage
+import json
 # Create your views here.
 def home(request):
     client = user_image.objects.all()
@@ -11,14 +12,20 @@ def home(request):
     return render(request , "home.html" , {"data": client})
 
 def save_data(request):
+    print(request.POST)
+    print(request.FILES)
     if request.method == "POST":
         name = request.POST.get('name')
+        fname = request.POST.get('file_name')
         file = request.FILES.get('file')
         fss  = FileSystemStorage()
+        # jfile=json.dumps(file)
+        # print(jfile)
         print(file ,name)
-        file_name = fss.save(file.name ,file )
-
+        file_name = fss.save(file.name, file)
+        print(file_name,'file_name')
         url = fss.url(file_name)
+        # print(url)
         user_image.objects.create(image = url , user_name = name)
         # print(request.FILES)
         print(name )
@@ -27,7 +34,7 @@ def save_data(request):
             # print(client_data.image)
         x = user_image.objects.values()
         client = list(x)
-        return JsonResponse({"link":url  ,  "client_data": client})
+        return JsonResponse({"client_data": client})
     else:
         return JsonResponse({"status":0})
         
